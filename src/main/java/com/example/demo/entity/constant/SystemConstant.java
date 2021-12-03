eb53971cf2b8b6171c105071e67cbacf219d71f0
yuan <2651123317@qq.com>
2021-12-03 00:33:44 -0500

    12-01-2021 add log out & front-end change

--- src/main/java/com/example/demo/entity/constant/SystemConstant.java
+++ src/main/java/com/example/demo/entity/constant/SystemConstant.java
@@ -2,4 +2,5 @@ package com.example.demo.entity.constant;
 
 public class SystemConstant {
     public static final String USER_ID = "user_id";
+    public static final String ACTIVE_URL = "activeUrl";
 }
--- src/main/java/com/example/demo/web/controller/PageController.java
+++ src/main/java/com/example/demo/web/controller/PageController.java
@@ -1,6 +1,10 @@
 package com.example.demo.web.controller;
 
 
+import com.example.demo.entity.CourseUser;
+import com.example.demo.service.CourseService;
+import com.example.demo.service.CourseUserService;
+import com.example.demo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +13,20 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import static com.example.demo.entity.constant.SystemConstant.USER_ID;
+import static com.example.demo.entity.constant.SystemConstant.ACTIVE_URL;
 
 @Controller
 public class PageController {
 
     @Resource
     private HttpSession httpSession;
+
+//    @Resource
+//    private UserService userService;
+//    @Resource
+//    private CourseService courseService;
+//    @Resource
+//    private CourseUserService userCourseService;
 
     @GetMapping(value = "/")
     public String index(){
@@ -30,8 +42,29 @@ public class PageController {
     }
 
     @GetMapping(value = "/dashboard")
-    public String dashboard() {
-
+    public String dashboard(Model model) {
+        httpSession.setAttribute(ACTIVE_URL, "dashboard");
+//        model.addAttribute("courses", courseService.count());
+//        model.addAttribute("students", userService.count());
+//        model.addAttribute("elective", userCourseService.count());
         return "dashboard";
+    }
+
+    @GetMapping(value = "/user")
+    public String user() {
+        httpSession.setAttribute(ACTIVE_URL, "user");
+        return "user";
+    }
+
+    @GetMapping(value = "/table")
+    public String table() {
+        httpSession.setAttribute(ACTIVE_URL, "table");
+        return "table";
+    }
+
+    @GetMapping("/logOut")
+    public String logOut() {
+        httpSession.removeAttribute(USER_ID);
+        return "redirect:/index";
     }
 }
