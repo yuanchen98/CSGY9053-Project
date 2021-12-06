d8de503f52521d26d2b39255251212d358c6bca1
yuan <2651123317@qq.com>
2021-12-06 15:53:38 -0500

    12-06-2021 add login interceptor

---
+++ src/main/java/com/example/demo/web/config/WebConfigurer.java
@@ -0,0 +1,29 @@
+package com.example.demo.web.config;
+
+import com.example.demo.web.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Resource
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/index","/api/user/login");
+    }
+}
+
---
+++ src/main/java/com/example/demo/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.example.demo.web.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.example.demo.entity.constant.SystemConstant.USER_ID;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // don't intercept the static resource
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
+        final Object obj = request.getSession().getAttribute(USER_ID);
+        if (obj == null) {
+            response.sendRedirect("/");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
